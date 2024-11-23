8d96e4107fa25f48d95086410a9647def4df45c5
Muhammad Abdullah <m.abdullah.fuf@gmail.com>
2024-11-23 19:56:11 +0500

    just work on array.php file

---
+++ array.php
@@ -0,0 +1,98 @@
+<?php 
+
+// Array 
+
+$fruits = ["apple", "mango", "banana", "grapes"];
+
+// $users = [];
+
+// echo $fruits[0]; 
+
+$numbers = [100, 200, 300, 400, 500];
+
+// echo $fruits[0]. $fruits[4];
+
+// $user = ["name" => "abdullah" , "email" => "abdullah@gmail.com" , "phone" => "01700000000" , "password" => "123456"];
+
+// echo "<pre>";
+// print_r($user);
+
+$user = [
+    ["name" => "abdullah" , "email" => "abdullah@gmail.com" , "phone" => "01700000000" , "password" => "123456"],
+    ["name" => "ahmed" , "email" => "ahmed@gmail.com" , "phone" => "01700000001" , "password" => "123456"],
+    ["name" => "amin" , "email" => "amin@gmail.com" , "phone" => "01700000002" , "password" => "123456"],
+    ["name" => "ali" , "email" => "ali@gmail.com" , "phone" => "01700000003" , "password" => "123456"],
+    ["name" => "abdullah" , "email" => "abdullah@gmail.com" , "phone" => "01700000000" , "password" => "123456"],
+    ["name" => "ahmed" , "email" => "ahmed@gmail.com" , "phone" => "01700000001" , "password" => "123456"],
+    ["name" => "amin" , "email" => "amin@gmail.com" , "phone" => "01700000002" , "password" => "123456"],
+    ["name" => "ali" , "email" => "ali@gmail.com" , "phone" => "01700000003" , "password" => "123456"],
+    ["name" => "abdullah" , "email" => "abdullah@gmail.com" , "phone" => "01700000000" , "password" => "123456"],
+    ["name" => "ahmed" , "email" => "ahmed@gmail.com" , "phone" => "01700000001" , "password" => "123456"],
+    ["name" => "amin" , "email" => "amin@gmail.com" , "phone" => "01700000002" , "password" => "123456"],
+    ["name" => "ali" , "email" => "ali@gmail.com" , "phone" => "01700000003" , "password" => "123456"],
+    ["name" => "abdullah" , "email" => "abdullah@gmail.com" , "phone" => "01700000000" , "password" => "123456"],
+    ["name" => "ahmed" , "email" => "ahmed@gmail.com" , "phone" => "01700000001" , "password" => "123456"],
+    ["name" => "amin" , "email" => "amin@gmail.com" , "phone" => "01700000002" , "password" => "123456"],
+    ["name" => "ali" , "email" => "ali@gmail.com" , "phone" => "01700000003" , "password" => "123456"],
+    ["name" => "abdullah" , "email" => "abdullah@gmail.com" , "phone" => "01700000000" , "password" => "123456"],
+    ["name" => "ahmed" , "email" => "ahmed@gmail.com" , "phone" => "01700000001" , "password" => "123456"],
+    ["name" => "amin" , "email" => "amin@gmail.com" , "phone" => "01700000002" , "password" => "123456"],
+    ["name" => "ali" , "email" => "ali@gmail.com" , "phone" => "01700000003" , "password" => "123456"],
+    ["name" => "abdullah" , "email" => "abdullah@gmail.com" , "phone" => "01700000000" , "password" => "123456"],
+    ["name" => "ahmed" , "email" => "ahmed@gmail.com" , "phone" => "01700000001" , "password" => "123456"],
+    ["name" => "amin" , "email" => "amin@gmail.com" , "phone" => "01700000002" , "password" => "123456"],
+    ["name" => "ali" , "email" => "ali@gmail.com" , "phone" => "01700000003" , "password" => "123456"],
+
+];
+
+// echo "<pre>";
+
+// echo $user[2]["email"];
+// print_r($user);
+
+
+// echo $user["phone"];
+
+
+// echo $numbers[0];
+
+// echo "<pre>";
+
+// print_r($fruits);
+
+// print_r($numbers);
+
+// $errors = "";
+
+
+
+// $user = "";
+
+// $user = "abdulalh" . "<br>";
+
+// echo $user;
+
+// $user = "waseem";
+
+// echo $user;
+
+
+
+
+$data = [];
+
+
+
+$data[] = "abdullah";
+$data[] = "ahmed";
+$data[] = "amin";
+$data[] = "ali";
+$data[] = "abdullah";
+$data[] = "ahmed";
+
+echo "<pre>";
+print_r($data);
+
+
+
+?>
